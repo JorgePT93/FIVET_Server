50c75e61327ed13725f89dae10aea6d335ad71ca
Jorge Ignacio Pizarro Tapia <jpt010@alumnos.ucn.cl>
2020-06-02 07:51:50 -0400

    refactor(FivetContext.cs): Coded FivetContext.

--- FivetContext.cs
+++ FivetContext.cs
@@ -1,7 +1,60 @@
-namespace fivet_server
+using System.Reflection;
+using Fivet.ZeroIce.model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fivet.Dao
 {
-    public class FivetContext
+    /// <summary>
+    /// The Connection to the FivetDatabase
+    /// </summary>
+    public class FivetContext : DbContext
     {
-        
+        /// <summary>
+        /// The Connection to the database
+        /// </summary>
+        public DbSet<Persona> Personas {get; set;}
+
+        /// <summary>
+        /// Configuration
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Using SQLite
+            optionsBuilder.UseSqLite("Data Source=fivet.db", options => 
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);                
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        /// <summary>
+        /// Create the ER from Entity
+        /// </summary>
+        /// <param name="modelBuilder">to use</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Update the model
+            modelBuilder.Entity<Persona>(p=>
+            {
+                // Primary key
+                p.HasKey(p => p.uid);
+                // Index in Email
+                p.Property(p => p.email).IsRequired();
+                p.HasIndex(p => p.email).IsUnique();
+            });
+
+            // Insert the data
+            modelBuilder.Entity<Persona>().HasData(
+                new Persona()
+                {
+                    uid = 1,
+                    nombre = "Jorge",
+                    apellido = "Pizarro",
+                    direccion = "Angamos #0618",
+                    email = "jpt010@alumnos.ucn.cl"
+                }
+            );
+        }
     }
 }
